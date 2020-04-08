cf423fd8ea6dd8832f250cef3287a9f9cc14e615
Saikiran Chepuri <saikiranchepuri35@gmail.com>
2020-04-08 17:38:53 +0530

    spark conf file

--- src/main/scala/com/epam/travel/Configuration.scala
+++ src/main/scala/com/epam/travel/Configuration.scala
@@ -1,8 +1,8 @@
-package com.epam.travel
+package com.epam.sales
 
 import org.apache.spark.sql.SparkSession
 
 trait Configuration {
-  val spark = SparkSession.builder().master("yarn").appName("Travel-MetaRecommandation")
+  val spark = SparkSession.builder().master("yarn").appName("SalesInfo")
     .config("spark.sql.warehouse.dir","/apps/hive/warehouse").config("hive.metastore.uris","thrift://rm01.itversity.com:9083").enableHiveSupport().getOrCreate()
 }
